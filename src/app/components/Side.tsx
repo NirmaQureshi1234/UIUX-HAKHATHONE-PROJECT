0a7f73f138e0f4af383e7c275cf502b82cb2df0d
Nirma Qureshi <nirmaqureshi786@gmail.com>
2024-12-10 05:34:15 -0800

    Update Side.tsx

--- src/app/components/Side.tsx
+++ src/app/components/Side.tsx
@@ -1,33 +1,3 @@
-// import React from "react";
-
-// function Side() {
-//   return (
-//     <>
-//       <div className="container flex flex-wrap justify-center py-[50px] bg-[#FAF4F4] h-[672px] ">
-//         <div className="w-[605px] h-[562]">
-//           <img src="/images/side-table.png" alt="" className="h-[500px]" />
-//           <p className="text-[36px] text-black">Side Table</p>
-//           <p className="underline  text-black text-[24px] w-[121px] border-b-[2px]">
-//             View More
-//           </p>
-//         </div>
-//         <div className="w-[605px] h-[562]">
-//           <img src="/images/sofa-seater.png" alt="" className="h-[500px]" />
-//           <p className="text-[36px] text-black">Side Table</p>
-//           <p className="underline text-[24px] text-black">View More</p>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Side;
-
- 
-
-
-
-  
 "use client"
 import React from "react";
 
